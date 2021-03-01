0a493da92f3c6704ccb9f45c39d3be1355bcbf1b
Utkarsha Dumane <uvdumane@gmail.com>
2021-03-01 18:27:50 +0530

    UD: Image upload changes

--- BikeTrade/src/main/java/com/biketrade/controller/SolrController.java
+++ BikeTrade/src/main/java/com/biketrade/controller/SolrController.java
@@ -1,9 +1,12 @@
 package com.biketrade.controller;
 
  
-import org.springframework.beans.factory.annotation.Autowired; 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.solr.core.SolrTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,10 +20,23 @@ public class SolrController {
 	@Autowired
 	BTSolrRepository btSolrRepository;
 	
-    @GetMapping("/{id}")
-    public Iterable<SBike> getBook(@PathVariable int id) {
-        return btSolrRepository.findAll();
+	@Autowired
+	SolrTemplate solrTemplate;
+	
+    @GetMapping("/{filter}/{start}/{end}")
+    public Iterable<SBike> getBikes(@PathVariable String filter,@PathVariable Double start,@PathVariable Double end) {
+    	 
+        return btSolrRepository.getBikes(filter,start,end);
     }
+    
+    
+    @PostMapping("/save")
+    public SBike saveBike(@RequestBody SBike sbike) {  
+    	
+    	System.out.println(sbike);
+    	return btSolrRepository.save(sbike);    	
+    }
+    
 
  
 }
--- BikeTrade/src/main/java/com/biketrade/dao/BTSolrRepository.java
+++ BikeTrade/src/main/java/com/biketrade/dao/BTSolrRepository.java
@@ -1,4 +1,6 @@
 package com.biketrade.dao; 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.solr.repository.Query;
 import org.springframework.data.solr.repository.SolrCrudRepository;
@@ -6,15 +8,17 @@ import org.springframework.stereotype.Repository;
 
 import com.biketrade.pojo.SBike;
 
-import java.util.List;
-
  
 
 @Repository
 @Qualifier("bikeSolrRepository")
 public interface BTSolrRepository extends SolrCrudRepository<SBike, Long> {
 
-    @Query(value = "*:*")
-    List<SBike> getBikes();
+    @Query(value = "fq = brand:?0 & fq = running:[?1 TO ?2]")
+    List<SBike> getBikes(String brand,Double start,Double end);
+    
+    List<SBike> findByBrandAndRunningBetween(String brand,String start,String end);
+    
+  //  List<SBike> findByModelNameAndfindByBrand(String modelName,String location);
 
 } 
